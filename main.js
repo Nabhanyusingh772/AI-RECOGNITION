a49dd0ff135a45e8eca634aeeb2a93729be2ebd6
Nabhanyusingh772 <84568333+Nabhanyusingh772@users.noreply.github.com>
2022-01-15 12:39:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristX=0;
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,500);
+    canvas.position(560,150);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log('PoseNet is Initialized!');
+}
+function gotPoses(results){
+    if(results.length>0){
+        noseX=results[0].pose.nose.x;
+         noseY=results[0].pose.nose.y;
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+         }
+}
+function draw(){
+    background('#969A97');
+    
+    document.getElementById("square_side")
+    fill('#F90093')
+    stroke('#F90093');
+    square(noseX,noseY,difference);
+}
